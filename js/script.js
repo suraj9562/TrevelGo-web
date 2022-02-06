b0f992be0951c031791379437c648b2ca8fcf320
suraj9562 <wadikarsuraj02@gmail.com>
2022-02-07 00:20:21 +0530

    done with Project

--- js/script.js
+++ js/script.js
@@ -60,3 +60,23 @@ var swiper = new Swiper(".review-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".brand-slider", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
